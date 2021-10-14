9c169d78194356bf1e6e9f806dc7c293930a96f0
Jesús Agudo <jagudo2514@gmail.com>
2021-10-13 19:19:20 -0500

    bug fix: db configuration - a bug in the database configuration has been fixed.

--- app.php
+++ app.php
@@ -1,17 +1,17 @@
 <?php
-require_once __DIR__.'./config/config.php';
-require_once __DIR__.'./classes/database.php';
-require_once __DIR__.'./interfaces/imodel.php';
-require_once __DIR__.'./classes/model.php';
-require_once __DIR__.'./classes/user.php';
-require_once __DIR__.'./classes/customer.php';
-require_once __DIR__.'./classes/reasonvisit.php';
-require_once __DIR__.'./classes/area.php';
-require_once __DIR__.'./classes/visit.php';
-require_once __DIR__.'./classes/report.php';
-require_once __DIR__.'./classes/observation.php';
-require_once __DIR__.'./classes/visitarea.php';
-require_once __DIR__.'./vendor/autoload.php';
+require_once __DIR__.'/config/config.php';
+require_once __DIR__.'/classes/database.php';
+require_once __DIR__.'/interfaces/imodel.php';
+require_once __DIR__.'/classes/model.php';
+require_once __DIR__.'/classes/user.php';
+require_once __DIR__.'/classes/customer.php';
+require_once __DIR__.'/classes/reasonvisit.php';
+require_once __DIR__.'/classes/area.php';
+require_once __DIR__.'/classes/visit.php';
+require_once __DIR__.'/classes/report.php';
+require_once __DIR__.'/classes/observation.php';
+require_once __DIR__.'/classes/visitarea.php';
+require_once __DIR__.'/vendor/autoload.php';
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
